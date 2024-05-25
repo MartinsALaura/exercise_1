3af21f243dca8a96ea278b13801614ee36a4bfff
MartinsALaura <lauraalvesmartins04@gmail.com>
2024-05-25 13:11:36 -0300

    Update Cliente class adding a Livro and a toString Method

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,18 +1,27 @@
 /* Cliente:
-Crie uma classe Cliente, que possui nome, idade, sexo, email e senha. Crie um construtor e os métodos de acesso dos atributos.
+Crie uma classe Cliente, que possui nome, idade, sexo, email, senha e um livro. Crie um construtor e os métodos de acesso dos atributos.
 Na classe cliente, crie um método chamado imprimeInformacoes, que imprime as informações do cliente de forma legível e organizada.
+Crie nesta classe, também, o método toString.
 */
 
 public class Cliente {
     private String nome, email, senha, sexo;
     private int idade;
-    
-    public Cliente(String nome, int idade, String sexo, String email, String senha) {
+    private Livro livro;
+
+    @Override
+    public String toString() {
+        return "Cliente [nome=" + nome + ", email=" + email + ", senha=" + senha + ", sexo=" + sexo + ", idade=" + idade
+                + ", livro=" + livro.toString() + "]";
+    }
+
+    public Cliente(String nome, int idade, String sexo, String email, String senha, Livro livro) {
         this.nome = nome;
         this.idade = idade;
         this.sexo = sexo;
         this.email = email;
         this.senha = senha;
+        this.livro = livro;
     }
 
     public String getNome() {
@@ -54,10 +63,18 @@ public class Cliente {
     public void setSenha(String senha) {
         this.senha = senha;
     }
+    
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
 
     public void imprimeInformacoes() {
         System.out.println("_____________________________\n");
-        System.out.println("Nome: " + nome + "\nIdade: " + idade + "\nSexo: " + sexo + "\nEmail: " + email);
-        System.out.println("_____________________________\n");
+        System.out.println("Nome: " + nome + "\nIdade: " + idade + "\nSexo: " + sexo + "\nEmail: " + email + "\nLivro: ");
+        livro.imprimeLivro();
     }
 }
